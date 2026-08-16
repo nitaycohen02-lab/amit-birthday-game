2eb3584892cacb9ae742c01acd1ec380dfbdd049
nitaycohen02-lab <nitaycohen02@gmail.com>
2026-08-16 15:48:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='amit-birthday-v6';
+const CACHE='amit-birthday-v7';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./amit-cartoon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
