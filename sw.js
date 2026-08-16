f0901046dcec5227cebcc18a219a23f97f4bd9f5
nitaycohen02-lab <nitaycohen02@gmail.com>
2026-08-16 14:32:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE='amit-birthday-v5-final';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='amit-birthday-v6';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./amit-cartoon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-   e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',x));return r}).catch(()=>caches.match('./index.html')));
-   return;
- }
- e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',x));return r}).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
